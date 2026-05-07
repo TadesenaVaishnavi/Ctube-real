3b927d8673594c6006db5533b3541f970a6754b7
Book <160591205+TadesenaVaishnavi@users.noreply.github.com>
2026-05-08 00:15:40 +0530

    new change

--- Frontend/src/components/Channelpage.jsx
+++ Frontend/src/components/Channelpage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./channelpage.css";
+import "../styles/Channelpage.css";
 
 const ChannelPage = () => {
   const navigate = useNavigate();
